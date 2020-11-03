057f8df9b97e89501eb49a82c5c47e135ed59cb2
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-02 18:30:06 -0600

    automatic login after signup

--- themes/models.py
+++ themes/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Theme(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
--- themes/views.py
+++ themes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Theme
 
 def home(request):
@@ -10,13 +11,27 @@ def home(request):
     '''
     return render(request, 'themes/home.html')
 
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
 
 class CreateTheme(generic.CreateView):
     model = Theme
     fields = ['title', 'description']
     template_name = 'themes/create_theme.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateTheme, self).form_valid(form)
+        return redirect('home')
